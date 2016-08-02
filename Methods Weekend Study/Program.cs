375de099ca594504985fab97b96285ec784080b4
Alexis Chandler <alexiscchandler@gmail.com>
2016-08-02 17:37:40 -0400

    This was the original project

--- Methods Weekend Study/Program.cs
+++ Methods Weekend Study/Program.cs
@@ -4,67 +4,168 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Methods_Weekend_Study
+namespace phrases
 {
     class Program
     {
         static void Main(string[] args)
+
+
+        //Problem 1
         {
+
+            Phrasing("PRETTY PONIES AND RAINBOWS");
+        }
+        static void Phrasing(string value)
+        {
+
+
+
+            string reverse = string.Empty;
+
+            char chSplit = ' ';
+
+            string[] words = value.Split(new char[] { chSplit }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string word in words)
+
+            {
+                reverse = word + chSplit + reverse;
+
+            }
+            Console.WriteLine(reverse);
+            Console.ReadKey();
+
+
+        }
 
             
-            string[] phrase = Phrasing(int amount);
-            Reverse(phrase);
-            PrintPhrase(phrase);
-
-        }
 
 
 
-       
+        
 
-        static string[] Phrasing(int amount)
+        //Problem 2
+
         {
-            
-            string[] phrase = new string[amount];
-            for (int i = 0; i < amount; i++)
-            {
-                phrase[i] = Console.ReadLine() + 1;
-            }
-            return phrase;
-        }
+            Project("ALEXIS", "JUNE");
 
 
-        static void PrintPhrase(string[] phrase)
+
+    }
+
+    static string Project(string name, string month)
+    {
+
+
+
+
+
+        switch (month)
         {
-            for (int i = 0; i < phrase.Length; i++)
-            {
-                Console.WriteLine(phrase[i] + "");
-            }
+            case "FEBRUARY":
+                Console.WriteLine(name + " IS A " + month + " BABY!");
+                break;
+            case "MARCH":
+                Console.WriteLine(name + " IS A " + month + " BABY!");
+                break;
+            case "MAY":
+                Console.WriteLine(name + " IS A " + month + " BABY!");
+                break;
+            case "JUNE":
+                Console.WriteLine(name + " IS A " + month + " BABY!");
+                break;
+            case "JULY":
+                Console.WriteLine(name + " IS A " + month + " BABY!");
+                break;
+            case "SEPTEMBER":
+                Console.WriteLine(name + " IS A " + month + " BABY!");
+                break;
+            case "NOVEMBER":
+                Console.WriteLine(name + " IS A " + month + " BABY!");
+                break;
+            case "DECEMBER":
+                Console.WriteLine(name + " IS A " + month + " BABY!");
+                break;
+            case "APRIL":
+                Console.WriteLine(name + " IS AN " + month + " BABY!");
+                break;
+            case "AUGUST":
+                Console.WriteLine(name + " IS AN " + month + " BABY!");
+                break;
+            case "OCTOBER":
+                Console.WriteLine(name + " IS AN " + month + " BABY!");
+                break;
 
-            Console.WriteLine();
 
         }
 
+        string final = name + month;
+        return final;
+
+    }
+
+        
 
 
-
-        static void Reverse(string[] phrase)
+        //Problem 3
+        
         {
-            int firstI = 0;
-            int secondI = phrase.Length - 1;
+            Name("alexis ", "chandler");
+}
 
-            while (firstI < secondI)
-            {
-                string temp = phrase[firstI];
-                phrase[firstI] = phrase[secondI];
-                phrase[secondI] = temp;
-            }
 
-            firstI++;
-            secondI++;
-            
+static string Name(string first, string last)
+{
+    first = first[0].ToString().ToUpper() + first.Substring(1, first.Length - 1);
+    last = last[0].ToString().ToUpper() + last.Substring(1, last.Length - 1);
+    Console.WriteLine(first + last);
+    string final = first + last;
 
+    return final;
+
+
+
+}
+
+    
+
+        //Problem 4
+
+
+        {
+            RemoveRandom("it's Fr0iday!");
         }
+
+        static string RemoveRandom(string princess)
+{
+    if (princess.Contains("0"))
+    {
+        princess = princess.Replace("0", "");
+        Console.Write(princess);
+    }
+
+    else
+    {
+        Console.WriteLine("This string is ok!");
+    }
+
+
+    string queen = princess;
+    return queen;
+
+}
+    
+
+
+
+
+//problem 1 the correct way
+
+//made into string array. then split it, then reverse it. then foreach
+
+
+
+
 
     
     
